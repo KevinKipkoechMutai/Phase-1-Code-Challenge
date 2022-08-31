0b6951299f40f3afc2247e090761c1a320344e79
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-31 11:33:37 +0300

    Function to capture card info

--- src/index.js
+++ src/index.js
@@ -3,3 +3,24 @@
 let cardInfo;
 
 //Creating the card-rendering function
+function cardDisp(card) {
+    cardInfo = card;
+    cardTitle = document.querySelector('#card-title');
+    cardImageName = document.querySelector("#card-image");
+    cardLikes = document.getElementById("like-count");
+    cardLikesBtn = document.querySelector("#like-button");
+    cardCommentList = document.querySelector('#comments-list');
+    cardCommentForm = document.querySelector("#comment-form");
+    cardCommentInput = document.querySelector('#comment');
+
+
+    cardTitle.textContent = card.title;
+    cardImageName.src = card.image;
+    cardLikes.textContent = `${card.likes} Likes`;
+
+
+    while (cardCommentList.hasChildNodes()) {
+        cardCommentList.removeChild(cardCommentList.lastChild);
+    }
+}
+
