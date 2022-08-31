158186bb5965a5375886242f3a27f8ecc02d15d0
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-08-31 11:53:34 +0300

    Colecting and storing input data

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ let cardInfo;
 //Creating the card-rendering function
 function cardDisp(card) {
     cardInfo = card;
-    cardTitle = document.querySelector('#card-title');
-    cardImageName = document.querySelector("#card-image");
-    cardLikes = document.getElementById("like-count");
-    cardLikesBtn = document.querySelector("#like-button");
-    cardCommentList = document.querySelector('#comments-list');
-    cardCommentForm = document.querySelector("#comment-form");
-    cardCommentInput = document.querySelector('#comment');
+    cardTitle = document.querySelector('#card-title'),
+    cardImageName = document.querySelector("#card-image"),
+    cardLikes = document.getElementById("like-count"),
+    cardLikesBtn = document.querySelector("#like-button"),
+    cardCommentList = document.querySelector('#comments-list'),
+    cardCommentForm = document.querySelector("#comment-form"),
+    cardCommentInput = document.querySelector('#comment'),
 
 
     cardTitle.textContent = card.title;
@@ -32,7 +32,24 @@ card.comments.forEach(comment => {
 })
 
 //Likes increment
-cardLikesBtn.addEventListener('click', (e) => {
+cardLikesBtn.addEventListener('click', () => {
     card.likes += 1;
     cardLikes.textContent = `${card.likes} Likes`;
 })
+
+//Comment input on form
+cardCommentForm.addEventListener('submit', (e) => {
+    const commentInput = document.querySelector('#comment');
+    e.preventDefault();
+    let commentTag = document.createElement('li');
+    commentTag.textContent = cardCommentInput.value;
+    cardCommentList.appendChild(commentTag);
+
+    const newCom = {
+        id: card.comments.length + 1,
+        imageId: 1,
+        content: cardCommentInput.value
+    }
+    card.comments.push(newCom);
+})
+
